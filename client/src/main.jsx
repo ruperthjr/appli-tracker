10fcfafb298cf05258c7ea7eaea1ddb2da31b7ad
ruperthjr <ruperthnyagesoa@gmail.com>
2025-11-29 14:08:31 +0300

    feat(client): updated main React entry point & .gitignore files

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+document.documentElement.classList.add("dark");
+document.body.classList.add("theme-dark");
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
